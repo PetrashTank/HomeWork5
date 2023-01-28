8cd54f01f9f8d8f0245ded91b17147d3c47b40d3
Tatiana <zanka_16@mail.ru>
2023-01-28 17:29:49 +0800

    Задание 1 ДЗ5

---
+++ Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2
+*/
+
+
+int [] CreateArray()
+{
+int [] Array = new int [5];
+Random number = new Random();
+for (int i = 0; i < Array.Length; i++)
+{
+Array[i] = number.Next(100,999);
+}
+return Array;
+}
+int [] a = CreateArray();
+void DivByTwo(int [] array)
+{
+    int a = 0;
+    for (int i = 0; i < array.Length; i++)
+    if (array[i] % 2 == 0)
+    {
+        a++;
+    }
+   Console.WriteLine(a);
+}
+Console.Write($"Массив [{a[0]},{a[1]},{a[2]},{a[3]},{a[4]}] -> ");
+DivByTwo(a);
