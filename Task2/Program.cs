3e9342e909ea4009fb8b073e9eb63ee79cfde531
Tatiana <zanka_16@mail.ru>
2023-01-28 18:36:43 +0800

    Задание 2 ДЗ5

---
+++ Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Задайте одномерный массив, 
+заполненный случайными числами. 
+Найдите сумму элементов, 
+стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+
+[-4, -6, 89, 6] -> 0                             
+
+*/
+
+int [] CreateArray()
+{
+int [] Array = new int [5];
+Random number = new Random();
+for (int i = 0; i < Array.Length; i++)
+{
+Array[i] = number.Next(-999,999);
+}
+return Array;
+}
+
+int [] a = CreateArray();
+void SummOdd(int [] array)
+{
+    int a = 0;
+    for (int i = 0; i < array.Length; i++)
+    if (array[i] % 2 != 0)
+    {
+        a=array[i]+a;
+    }
+   Console.WriteLine(a);
+}
+Console.Write($"Массив [{a[0]},{a[1]},{a[2]},{a[3]},{a[4]}] -> ");
+SummOdd(a);
